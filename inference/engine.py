83c27c621c041aba38e3141cab87e08014b2307e
Jay <jaygala24@gmail.com>
2023-05-03 17:12:12 +0530

    fadd uuid temp file creation for standard inference engine

--- inference/engine.py
+++ inference/engine.py
@@ -160,17 +160,19 @@ class Model:
         assert isinstance(batch, list)
         
         # -------------------------------------------------------
+        fname = str(uuid.uuid4())
+        
         # normalize punctuations
-        with open("tmp.txt", "w", encoding="utf-8") as f:
+        with open(f"{fname}.txt", "w", encoding="utf-8") as f:
             f.write("\n".join(batch))
         
-        os.system(f"bash {PWD}/normalize_punctuation.sh {src_lang} < tmp.txt > tmp.txt._norm")
+        os.system(f"bash {PWD}/normalize_punctuation.sh {src_lang} < {fname}.txt > {fname}.txt._norm")
         
-        with open("tmp.txt._norm", "r", encoding="utf-8") as f:
+        with open(f"{fname}.txt._norm", "r", encoding="utf-8") as f:
             batch = f.read().split("\n")
             
-        os.unlink("tmp.txt")
-        os.unlink("tmp.txt._norm")
+        os.unlink(f"{fname}.txt")
+        os.unlink(f"{fname}.txt._norm")
         # -------------------------------------------------------
         
         preprocessed_sents = self.preprocess(batch, lang=src_lang)
