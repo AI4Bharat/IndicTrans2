e06fb4257f655aacef94679943ee1224072dec39
Pranjal Chitale <pranjalchitale@gmail.com>
2023-05-23 20:11:29 +0530

    Google and Azure Translate API evaluation scripts.

---
+++ baseline_eval/azure_translate.py
@@ -0,0 +1,185 @@
+import sys
+import glob
+import requests
+from urllib.parse import urlencode
+import traceback
+import time
+
+flores_to_iso = {
+    "asm_Beng": "as",
+    "ben_Beng": "bn",
+    "brx_Deva": "brx",
+    "doi_Deva": "doi",
+    "eng_Latn": "en",
+    "gom_Deva": "gom",
+    "guj_Gujr": "gu",
+    "hin_Deva": "hi",
+    "kan_Knda": "kn",
+    "kas_Arab": "ks",
+    "kas_Deva": "ks_Deva",
+    "mai_Deva": "mai",
+    "mal_Mlym": "ml",
+    "mar_Deva": "mr",
+    "mni_Beng": "mni_Beng",
+    "mni_Mtei": "mni",
+    "npi_Deva": "ne",
+    "ory_Orya": "or",
+    "pan_Guru": "pa",
+    "san_Deva": "sa",
+    "sat_Olck": "sat",
+    "snd_Arab": "sd",
+    "snd_Deva": "sd_Deva",
+    "tam_Taml": "ta",
+    "tel_Telu": "te",
+    "urd_Arab": "ur",
+}
+
+
+class AzureTranslator:
+    def __init__(
+        self,
+        subscription_key: str,
+        region: str,
+        endpoint: str = "https://api.cognitive.microsofttranslator.com",
+    ) -> None:
+        self.http_headers = {
+            "Ocp-Apim-Subscription-Key": subscription_key,
+            "Ocp-Apim-Subscription-Region": region,
+        }
+        self.translate_endpoint = endpoint + "/translate?api-version=3.0&"
+        self.languages_endpoint = endpoint + "/languages?api-version=3.0"
+
+        self.supported_languages = self.get_supported_languages()
+
+    def get_supported_languages(self) -> dict:
+        return requests.get(self.languages_endpoint).json()["translation"]
+
+    def batch_translate(self, texts: list, src_lang: str, tgt_lang: str) -> list:
+        if not texts:
+            return texts
+
+        src_lang = flores_to_iso[src_lang]
+        tgt_lang = flores_to_iso[tgt_lang]
+
+        if src_lang not in self.supported_languages:
+            raise NotImplementedError(
+                f"Source language code: `{src_lang}` not supported!"
+            )
+
+        if tgt_lang not in self.supported_languages:
+            raise NotImplementedError(
+                f"Target language code: `{tgt_lang}` not supported!"
+            )
+
+        body = [{"text": text} for text in texts]
+        query_string = urlencode(
+            {
+                "from": src_lang,
+                "to": tgt_lang,
+            }
+        )
+
+        try:
+            response = requests.post(
+                self.translate_endpoint + query_string,
+                headers=self.http_headers,
+                json=body,
+            )
+        except:
+            traceback.print_exc()
+            return None
+
+        try:
+            response = response.json()
+        except:
+            traceback.print_exc()
+            print("Response:", response.text)
+            return None
+
+        return [payload["translations"][0]["text"] for payload in response]
+
+    def text_translate(self, text: str, src_lang: str, tgt_lang: str) -> str:
+        return self.batch_translate([text], src_lang, tgt_lang)[0]
+
+
+if __name__ == "__main__":
+    import os
+    from dotenv import dotenv_values
+
+    root_dir = sys.argv[1]
+    
+    # Expects a .env file containing the API credentials.
+
+    config = dotenv_values(os.path.join(os.path.dirname(__file__), ".env"))
+
+    t = AzureTranslator(
+        config["AZURE_TRANSLATOR_TEXT_SUBSCRIPTION_KEY"],
+        config["AZURE_TRANSLATOR_TEXT_REGION"],
+        config["AZURE_TRANSLATOR_TEXT_ENDPOINT"],
+    )
+
+    pairs = sorted(glob.glob(os.path.join(root_dir, "*")))
+
+    for i, pair in enumerate(pairs):
+        basename = os.path.basename(pair)
+        
+        print(pair)
+    
+        src_lang, tgt_lang = basename.split("-")
+
+        print(f"{src_lang} - {tgt_lang}")
+
+        # source to target translations
+        src_infname = os.path.join(pair, f"test.{src_lang}")
+        tgt_outfname = os.path.join(pair, f"test.{tgt_lang}.pred.azure")
+        if not os.path.exists(src_infname):
+            continue
+
+        src_sents = [
+            sent.replace("\n", "").strip()
+            for sent in open(src_infname, "r").read().split("\n")
+            if sent
+        ]
+
+        if not os.path.exists(tgt_outfname):
+            try:
+                translations = []
+                for i in range(0, len(src_sents), 128):
+                    start, end = i, int(min(i + 128, len(src_sents)))
+                    translations.extend(
+                        t.batch_translate(src_sents[start:end], src_lang, tgt_lang)
+                    )
+                with open(tgt_outfname, "w") as f:
+                    f.write("\n".join(translations))
+                
+                time.sleep(10)
+            except Exception as e:
+                print(e)
+                continue
+
+        # target to source translations
+        tgt_infname = os.path.join(pair, f"test.{tgt_lang}")
+        src_outfname = os.path.join(pair, f"test.{src_lang}.pred.azure")
+        if not os.path.exists(tgt_infname):
+            continue
+
+        tgt_sents = [
+            sent.replace("\n", "").strip()
+            for sent in open(tgt_infname, "r").read().split("\n")
+            if sent
+        ]
+
+        if not os.path.exists(src_outfname):
+            try:
+                translations = []
+                for i in range(0, len(tgt_sents), 128):
+                    start, end = i, int(min(i + 128, len(tgt_sents)))
+                    translations.extend(
+                        t.batch_translate(tgt_sents[start:end], tgt_lang, src_lang)
+                    )
+                with open(src_outfname, "w") as f:
+                    f.write("\n".join(translations))
+            except Exception as e:
+                continue
+
+            time.sleep(10)
---
+++ baseline_eval/google_translate.py
@@ -0,0 +1,128 @@
+import os
+import sys
+import glob
+from tqdm import tqdm
+from google.cloud import translate
+
+# Expects a json file containing the API credentials.
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(
+    os.path.dirname(__file__), r"api_key.json"
+)
+
+flores_to_iso = {
+    "asm_Beng": "as",
+    "ben_Beng": "bn",
+    "doi_Deva": "doi",
+    "eng_Latn": "en",
+    "gom_Deva": "gom",
+    "guj_Gujr": "gu",
+    "hin_Deva": "hi",
+    "kan_Knda": "kn",
+    "mai_Deva": "mai",
+    "mal_Mlym": "ml",
+    "mar_Deva": "mr",
+    "mni_Mtei": "mni_Mtei",
+    "npi_Deva": "ne",
+    "ory_Orya": "or",
+    "pan_Guru": "pa",
+    "san_Deva": "sa",
+    "sat_Olck": "sat",
+    "snd_Arab": "sd",
+    "tam_Taml": "ta",
+    "tel_Telu": "te",
+    "urd_Arab": "ur",
+}
+
+
+def translate_text(text, src_lang, tgt_lang, project_id="project_id"):
+
+    src_lang = flores_to_iso[src_lang]
+    tgt_lang = flores_to_iso[tgt_lang]
+
+    if src_lang == "mni_Mtei":
+        src_lang = "mni-Mtei"
+
+    if tgt_lang == "mni_Mtei":
+        tgt_lang = "mni-Mtei"
+
+    client = translate.TranslationServiceClient()
+
+    location = "global"
+
+    parent = f"projects/{project_id}/locations/{location}"
+
+    response = client.translate_text(
+        request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",  # mime types: text/plain, text/html
+            "source_language_code": src_lang,
+            "target_language_code": tgt_lang,
+        }
+    )
+
+    translated_text = ""
+    for translation in response.translations:
+        translated_text += translation.translated_text
+
+    return translated_text
+
+
+if __name__ == "__main__":
+    root_dir = sys.argv[1]
+
+    pairs = sorted(glob.glob(os.path.join(root_dir, "*")))
+
+    for pair in pairs:
+
+        print(pair)
+
+        basename = os.path.basename(pair)
+
+        src_lang, tgt_lang = basename.split("-")
+        if src_lang not in flores_to_iso.keys() or tgt_lang not in flores_to_iso.keys():
+            continue
+
+        if src_lang == "eng_Latn":
+            lang = tgt_lang
+        else:
+            lang = src_lang
+
+        lang = flores_to_iso[lang]
+
+        if lang not in "as bn doi gom gu hi kn mai ml mni_Mtei mr ne or pa sa sd ta te ur":
+            continue
+
+        print(f"{src_lang} - {tgt_lang}")
+
+        # source to target translations
+
+        src_infname = os.path.join(pair, f"test.{src_lang}")
+        tgt_outfname = os.path.join(pair, f"test.{tgt_lang}.pred.google")
+        if os.path.exists(src_infname) and not os.path.exists(tgt_outfname):
+            src_sents = [
+                sent.replace("\n", "").strip()
+                for sent in open(src_infname, "r").read().split("\n")
+                if sent
+            ]
+            translations = [
+                translate_text(text, src_lang, tgt_lang).strip() for text in tqdm(src_sents)
+            ]
+            with open(tgt_outfname, "w") as f:
+                f.write("\n".join(translations))
+
+        # # target to source translations
+        tgt_infname = os.path.join(pair, f"test.{tgt_lang}")
+        src_outfname = os.path.join(pair, f"test.{src_lang}.pred.google")
+        if os.path.exists(tgt_infname) and not os.path.exists(src_outfname):
+            tgt_sents = [
+                sent.replace("\n", "").strip()
+                for sent in open(tgt_infname, "r").read().split("\n")
+                if sent
+            ]
+            translations = [
+                translate_text(text, tgt_lang, src_lang).strip() for text in tqdm(tgt_sents)
+            ]
+
+            with open(src_outfname, "w") as f:
+                f.write("\n".join(translations))
