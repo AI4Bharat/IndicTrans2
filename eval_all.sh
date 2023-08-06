3de255b4abeb391475ac722328cd648a8077d6d7
VarunGumma <varun230999@gmail.com>
2023-08-06 10:31:54 +0530

    removed parallel processing for multi-benchmark eval

--- eval_all.sh
+++ eval_all.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 mkdir -p logs
-rm ../distillation_data/IN22/conv/*.csv ../distillation_data/IN22/gen/*.csv ../distillation_data/flores22/*.csv
+rm ../distillation_data/IN22/conv/*.csv ../distillation_data/IN22/gen/*.csv ../distillation_data/flores22/*.csv logs/*
 
-echo -e "\nworking on itv2_1"
-CUDA_VISIBLE_DEVICES=0 bash eval.sh ../distillation_data/IN22/conv ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed itv2_1 > logs/conv1.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 bash eval.sh ../distillation_data/IN22/gen ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed itv2_1 > logs/gen1.log 2>&1 &
-CUDA_VISIBLE_DEVICES=2 bash eval.sh ../distillation_data/flores22 ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed itv2_1 > logs/flores1.log 2>&1 &
-wait
+bash eval.sh ../distillation_data/IN22/conv ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed itv2_1 > logs/conv1.log 2>&1
+bash eval.sh ../distillation_data/IN22/conv ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed_seed_ft itv2_2 > logs/conv2.log 2>&1
 
-echo -e "\nworking on itv2_2"
-CUDA_VISIBLE_DEVICES=0 bash eval.sh ../distillation_data/IN22/conv ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed_seed_ft itv2_2 > logs/conv2.log 2>&1 &
-CUDA_VISIBLE_DEVICES=1 bash eval.sh ../distillation_data/IN22/gen ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed_seed_ft itv2_2 > logs/gen2.log 2>&1 &
-CUDA_VISIBLE_DEVICES=2 bash eval.sh ../distillation_data/flores22 ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed_seed_ft itv2_2 > logs/flores2.log 2>&1 &
-wait
+bash eval.sh ../distillation_data/IN22/gen ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed itv2_1 > logs/gen1.log 2>&1
+bash eval.sh ../distillation_data/IN22/gen ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed_seed_ft itv2_2 > logs/gen2.log 2>&1
+
+bash eval.sh ../distillation_data/flores22 ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed itv2_1 > logs/flores1.log 2>&1
+bash eval.sh ../distillation_data/flores22 ../distillation_data/BPCC0_indic_en_bin base18L_dec_shared_embed_seed_ft itv2_2 > logs/flores2.log 2>&1
