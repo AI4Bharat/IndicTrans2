709573498804174f8c681c890435fc0bf4577346
Varun Gumma <45076943+VarunGumma@users.noreply.github.com>
2024-03-29 12:08:49 +0530

    Update train_lora.sh

--- huggingface_interface/train_lora.sh
+++ huggingface_interface/train_lora.sh
@@ -3,9 +3,9 @@ export CUDA_VISIBLE_DEVICES=0
 data_dir=${1:-"en-indic-exp"}
 model_name=${2:-"ai4bharat/indictrans2-en-indic-dist-200M"}
 output_dir=${3:-"output"}
-direction=${3:-"en-indic"}
-src_lang_list=${4:-"eng_Latn"}
-tgt_lang_list=${5:-"asm_Beng,ben_Beng,guj_Gujr,hin_Deva,kan_Knda,mal_Mlym,mar_Deva,npi_Deva,ory_Orya,pan_Guru,tam_Taml,tel_Telu,urd_Arab"}
+direction=${4:-"en-indic"}
+src_lang_list=${5:-"eng_Latn"}
+tgt_lang_list=${6:-"asm_Beng,ben_Beng,guj_Gujr,hin_Deva,kan_Knda,mal_Mlym,mar_Deva,npi_Deva,ory_Orya,pan_Guru,tam_Taml,tel_Telu,urd_Arab"}
 
 python3 train_lora.py \
     --data_dir $data_dir \
@@ -28,11 +28,11 @@ python3 train_lora.py \
     --num_workers 16 \
     --metric_for_best_model eval_BLEU \
     --greater_is_better \
-    --mixed_precision fp16 \
+    --mixed_precision bf16 \
     --patience 10 \
     --label_smoothing 0.1 \
     --lora_target_modules "q_proj,k_proj,v_proj" \
     --lora_dropout 0.05 \
     --lora_r 8 \
-    --lora_alpha 32 \
-    --generation_config '{"max_new_tokens": 256, "min_length": 1, "num_beams": 5, "use_cache": true, "num_return_sequences": 1}'
+    --lora_alpha 16 \
+    --generation_config '{"max_new_tokens": 256, "min_length": 1, "num_beams": 5, "use_cache": true, "num_return_sequences": 1, "decoder_start_token_id": 2}'
